47ddb635589ef7bf101d2420465dc01844de190d
Muhammad syahrur rahmansyah moha <mrjunior403@gmail.com>
2022-11-21 21:58:45 +0700

    sql1

---
+++ function.php
@@ -0,0 +1,12 @@
+<?php 
+
+function query($query){
+	global $koneksi;
+	$sql = $koneksi->query($query);
+	$fetch = [];
+	while ($fetchs = $sql->fetch_assoc()) {
+		$fetch = $fetchs;
+	}
+	return $fetch;
+}
+ ?>
