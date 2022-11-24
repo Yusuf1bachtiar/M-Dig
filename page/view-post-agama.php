be4975fdd186da250d32d4bb1335484468a7f9bf
Yusuf1bachtiar <yusuf1bachtiar@gmail.com>
2022-11-24 19:18:37 +0700

    statistik

--- page/view-post-agama.php
+++ page/view-post-agama.php
@@ -1,7 +1,63 @@
 <!DOCTYPE html>
 <html>
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/modules/variable-pie.js"></script>
+<script src="https://code.highcharts.com/modules/exporting.js"></script>
+<script src="https://code.highcharts.com/modules/export-data.js"></script>
+<script src="https://code.highcharts.com/modules/accessibility.js"></script>
 <style>
+#container {
+  height: 500px;
+}
+
+.highcharts-figure,
+.highcharts-data-table table {
+  min-width: 320px;
+  max-width: 700px;
+  margin: 1em auto;
+}
+
+.highcharts-data-table table {
+  font-family: Verdana, sans-serif;
+  border-collapse: collapse;
+  border: 1px solid #ebebeb;
+  margin: 10px auto;
+  text-align: center;
+  width: 100%;
+  max-width: 500px;
+}
+
+.highcharts-data-table caption {
+  padding: 1em 0;
+  font-size: 1.2em;
+  color: #555;
+}
+
+.highcharts-data-table th {
+  font-weight: 600;
+  padding: 0.5em;
+}
+
+.highcharts-data-table td,
+.highcharts-data-table th,
+.highcharts-data-table caption {
+  padding: 0.5em;
+}
+
+.highcharts-data-table thead tr,
+.highcharts-data-table tr:nth-child(even) {
+  background: #f8f8f8;
+}
+
+.highcharts-data-table tr:hover {
+  background: #f1f7ff;
+}
+.chart.a {
+	background-color: #faf5f5; border-radius:10px;
+}
+.tabel-a {
+	background-color: #faf5f5; border-radius:10px;
+}
 table {
   font-family: arial, sans-serif;
   border-collapse: collapse;
@@ -19,96 +75,119 @@ tr:nth-child(even) {
 }
 </style>
 <body>
-<div class="card mb-4">
-                    <div class="card-body">
-                        <h2 class="card-title">Data Demografi Berdasarkan Agama</h2><hr>
-                    </div>
-                </div>
-<div
-id="myChart" style="width:100%; max-width:600px; height:500px;">
+<h2 style="margin-left: 15px;">DATA DEMOGRAFI BERDASARKAN AGAMA</h2>
+<hr style="border: 15px solid gray;border-radius: 15%">
+<div class="chart-a">
+<figure class="highcharts-figure">
+  <div id="container"></div>
+  <p class="highcharts-description">
+    Variable radius pie charts can be used to visualize a
+    second dimension in a pie chart. In this chart, the more
+    densely populated countries are drawn further out, while the
+    slice width is determined by the size of the country.
+  </p>
+</figure>
 </div>
-<div><h2>Table Agama</h2>
-	<table>
-		<tr>
-			<th class="tbl-st-hd" style="width: 5%;">Kode</th>
-			<th class="tbl-st-hd">Kelompok</th>
-			<th class="tbl-st-hd">Jumlah</th>
-			<th class="tbl-st-hd">Persentase</th>
-		</tr>
-		<tr>
-			<td>1</td>
-			<td>ISLAM</td>
-			<td>500</td>
-			<td>25%</td>
-		</tr>
-		<tr>
-			<td>2</td>
-			<td>ISLAM</td>
-			<td>500</td>
-			<td>25%</td>
-		</tr>
-		<tr>
-			<td>3</td>
-			<td>Kristen</td>
-			<td>500</td>
-			<td>25%</td>
-		</tr>
-		<tr>
-			<td>4</td>
-			<td>Khatolk</td>
-			<td>500</td>
-			<td>25%</td>
-		</tr>
-		<tr>
-			<td>5</td>
-			<td>Hindu</td>
-			<td>500</td>
-			<td>25%</td>
-		</tr>
-		<tr>
-			<td>5</td>
-			<td>Matahari</td>
-			<td>500</td>
-			<td>25%</td>
-		</tr>
-		<tr>
-			<td>6</td>
-			<td>ISLAM</td>
-			<td>500</td>
-			<td>25%</td>
-		</tr>
-		<tr>
-			<td>7</td>
-			<td>ISLAM</td>
-			<td>500</td>
-			<td>25%</td>
-		</tr>
-	</table>
+<div class="tabel-a">
+<h2>TABEL AGAMA</h2>
+<table>
+	<tr>
+		<th>Kode</th>
+		<th>Kelompok</th>
+		<th>Jumlah (Jiwa)</th>
+		<th>Persentase</th>
+	</tr>
+	<tr>
+		<td>1</td>
+		<td>Islam</td>
+		<td>510</td>
+		<td>91%</td>
+	</tr>
+	<tr>
+		<td>2</td>
+		<td>Islam</td>
+		<td>510</td>
+		<td>91%</td>
+	</tr>
+	<tr>
+		<td>3</td>
+		<td>Islam</td>
+		<td>510</td>
+		<td>91%</td>
+	</tr>
+	<tr>
+		<td>4</td>
+		<td>Islam</td>
+		<td>510</td>
+		<td>91%</td>
+	</tr>
+	<tr>
+		<td>5</td>
+		<td>Islam</td>
+		<td>510</td>
+		<td>91%</td>
+	</tr>
+	<tr>
+		<td>6</td>
+		<td>Islam</td>
+		<td>510</td>
+		<td>91%</td>
+	</tr>
+
+</table>
 </div>
-<script>
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
-
-function drawChart() {
-var data = google.visualization.arrayToDataTable([
-  ['Contry', 'Mhl'],
-  ['Italy',54.8],
-  ['France',48.6],
-  ['Spain',44.4],
-  ['USA',23.9],
-  ['Argentina',14.5]
-]);
-
-var options = {
-  title:'World Wide Wine Production',
-  is3D:true
-};
-
-var chart = new google.visualization.PieChart(document.getElementById('myChart'));
-  chart.draw(data, options);
-}
+<script type="text/javascript">
+	// Data retrieved from https://worldpopulationreview.com/country-rankings/countries-by-density
+Highcharts.chart('container', {
+  chart: {
+    type: 'variablepie'
+  },
+  title: {
+    text: 'Data Statistik Terkini Penduduk Berdasarkan laporan tahun 2021.'
+  },
+  tooltip: {
+    headerFormat: '',
+    pointFormat: '<span style="color:{point.color}">\u25CF</span> <b> {point.name}</b><br/>' +
+      'Area (square km): <b>{point.y}</b><br/>' +
+      'Population density (people per square km): <b>{point.z}</b><br/>'
+  },
+  series: [{
+    minPointSize: 10,
+    innerSize: '20%',
+    zMin: 0,
+    name: 'countries',
+    data: [{
+      name: 'Islam',
+      y: 505992,
+      z: 92
+    }, {
+      name: 'France',
+      y: 551695,
+      z: 119
+    }, {
+      name: 'Poland',
+      y: 312679,
+      z: 121
+    }, {
+      name: 'Czech Republic',
+      y: 78865,
+      z: 136
+    }, {
+      name: 'Italy',
+      y: 301336,
+      z: 200
+    }, {
+      name: 'Switzerland',
+      y: 41284,
+      z: 213
+    }, {
+      name: 'Germany',
+      y: 357114,
+      z: 235
+    }]
+  }]
+});
 </script>
-
 </body>
 </html>
 
