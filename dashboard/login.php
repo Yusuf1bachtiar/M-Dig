69ac45f12c27404269d8d80cdb0e3672e3803d77
Muhammad syahrur rahmansyah moha <mrjunior403@gmail.com>
2022-11-24 20:49:34 +0700

    fix search and cate

--- dashboard/login.php
+++ dashboard/login.php
@@ -19,18 +19,65 @@ if (isset($_COOKIE['login'])) {
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Login</title>
+	<link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/css/font-awesome.min.css" rel="stylesheet">
+    <link href="../assets/css/style.css" rel="stylesheet">
 </head>
 <body>
-<form method="post">
-	<label for="username">Username</label>
-	<input type="username" name="username">
-	<label for="password">Password</label>	
-	<input type="password" name="password">
-	<input type="checkbox" name="remember" value="remember">
-	<label for="remember">Remember Me</label>
-	<button type="submit" name="submit">Login</button>
-</form>
-</body>
+    <section class="form-02-main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="_lk_de">
+              <div class="form-03-main">
+                <div class="logo">
+                  <img src="../assets/images/user.png">
+                </div>
+                <form method="post">
+                <div class="form-group">
+                  <input type="username" name="username" class="form-control _ge_de_ol" type="text" placeholder="Enter username" required="" aria-required="true">
+                </div>
+
+                <div class="form-group">
+                  <input type="password" name="password" class="form-control _ge_de_ol" type="text" placeholder="Enter Password" required="" aria-required="true">
+                </div>
+
+                <div class="checkbox form-group">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" value="remember" id="">
+                    <label class="form-check-label" for="remember">
+                      Remember me
+                    </label>
+                  </div>
+                  <a href="#">Forgot Password</a>
+                </div>
+
+                <div class="form-group">
+                  <div class="_btn_04">
+                    <button type="submit" name="submit" style="background-color: transparent;border:none;text-decoration: none;">Login</button>
+                  </div>
+                </div>
+                </form>
+                <div class="form-group nm_lk"><p>Or Login With</p></div>
+
+                <div class="form-group pt-0">
+                  <div class="_social_04">
+                    <ol>
+                      <li><i class="fa fa-facebook"></i></li>
+                      <li><i class="fa fa-twitter"></i></li>
+                      <li><i class="fa fa-google-plus"></i></li>
+                      <li><i class="fa fa-instagram"></i></li>
+                      <li><i class="fa fa-linkedin"></i></li>
+                    </ol>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+   </body>
 </html>
 
 <?php if (isset($_POST['submit'])): 
